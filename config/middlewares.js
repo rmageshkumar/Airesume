910b10b324546e99269af6a2e5be94e8dac79524
Mageshkumar R <mageshkumar.it@gmail.com>
2025-03-06 12:36:14 +0000

    proxy

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,40 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:", "http:"],
+          "img-src": ["'self'", "data:", "blob:", "https:", "http:"],
+          "media-src": ["'self'", "data:", "blob:", "https:", "http:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [
+        "http://localhost:5173",
+        "http://localhost:1337",
+        "https://airesume-us8l.onrender.com",
+        "https://resumebuilder.technoread.net",
+      ],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
 ];
